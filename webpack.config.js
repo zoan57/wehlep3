618eeb1a14d7fc96892f0061ca0ad51843565136
zoan57 <zoan_c@icloud.com>
2023-03-10 01:49:55 +0800

    Initial

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path")
+module.exports = {
+    mode: "production",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        publicPath: "/",
+    },
+    devServer: {
+        static: "./dist",
+        allowedHosts: ['all'],
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }]
+    }
+
+}
